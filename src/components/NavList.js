5d7d6f9bb2506c6888deb7cda14a9695c7105cec
Anirudh Agarwal <anirudha58@gmail.com>
2023-11-25 00:20:49 +0530

    dynamic Sidebar

--- src/components/NavList.js
+++ src/components/NavList.js
@@ -1,27 +1,22 @@
 import React from "react";
 
-const NavList = () => {
+const NavList = ({ heading, list }) => {
   return (
     <div className="border-b border-gray-200 py-3 pr-4 w-[95%]">
-      <h2 className="text-xl font-semibold pb-3 pl-3 cursor-pointer">
-        Heading
-      </h2>
+      {heading && (
+        <h2 className="md:text-xl font-semibold pb-3 pl-3 cursor-pointer">
+          {heading}
+        </h2>
+      )}
       <ul className="list-none">
-        <li className="py-3 hover:bg-gray-300 rounded-md pl-3 cursor-pointer">
-          Your channel
-        </li>
-        <li className="py-3 hover:bg-gray-300 rounded-md pl-3 cursor-pointer">
-          History
-        </li>
-        <li className="py-3 hover:bg-gray-300 rounded-md pl-3 cursor-pointer">
-          Your videos
-        </li>
-        <li className="py-3 hover:bg-gray-300 rounded-md pl-3 cursor-pointer">
-          Watch Later
-        </li>
-        <li className="py-3 hover:bg-gray-300 rounded-md pl-3 cursor-pointer">
-          Downloads
-        </li>
+        {list.map((item, index) => (
+          <li
+            key={index}
+            className="py-3 hover:bg-gray-300 rounded-md pl-3 cursor-pointer text-sm md:text-base"
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,14 +14,31 @@ const Sidebar = () => {
   return (
     <div
       className={
-        "absolute lg:fixed z-30 top-[66px] h-screen py-2 overflow-y-auto bg-slate-100 transition-width duration-150 ease-in-out " +
+        "absolute lg:fixed z-30 top-0 pt-[66px] h-screen py-2 overflow-y-auto bg-slate-100 transition-width duration-150 ease-in-out " +
         sideBarDynamicStyle
       }
       id="sideBar"
     >
-      <NavList />
-      <NavList />
-      <NavList />
+      <NavList
+        heading=""
+        list={["Home", "Shorts", "Subscriptions", "Youtube Music"]}
+      />
+      <NavList
+        heading="You >"
+        list={["Your Channel", "History", "Your Videos", "Watch Later"]}
+      />
+      <NavList
+        heading="Subscriptions"
+        list={[
+          "Abhi and Niyu",
+          "5ocial",
+          "Aakash Chopra",
+          "Akshat Srivastav",
+          "Beer Biceps",
+          "Business Insider",
+          "Charisma On Command",
+        ]}
+      />
     </div>
   );
 };
